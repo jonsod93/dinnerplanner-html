e368ae49edde88f47fc68e99a881ccba8c046edd
lizoliz <lisatorekull@gmail.com>
2015-02-13 14:04:13 +0100

    De sista detaljerna samt startsida

--- js/view/exampleView.js
+++ js/view/exampleView.js
@@ -1,17 +1,21 @@
 //ExampleView Object constructor
 var ExampleView = function (container, model) {
 	
+
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
 	this.numberOfGuests = container.find("#numberOfGuests");
 	this.plusButton = container.find("#plusGuest");
 	this.minusButton = container.find("#minusGuest");
 	this.dishPrice = container.find("#dishPrice");
+	this.totalPrice = container.find("#totalPrice");
 	this.dishName = container.find("#dishName");
 	this.Dishes = container.find("#Dishes");
 
+	model.addDishToMenu(1);
 	this.dishPrice.html(model.getPrice(1));
 	this.dishName.html(model.getDishName(1));
 	this.numberOfGuests.html(model.getNumberOfGuests);
+	this.totalPrice.html(model.getTotalMenuPrice());
  
 }
---
+++ js/view/startView.js
@@ -0,0 +1,46 @@
+//ExampleView Object constructor
+var FinalView = function (container, model) {
+	
+	model.addDishToMenu(1);
+	model.addDishToMenu(100);
+	model.addDishToMenu(200);
+
+	var ChosenMenu = container.find("#ChosenMenu");
+
+
+	this.getMenu = function(){
+
+		
+		var stuff = '';
+		var menu = model.getFullMenu();
+		var menuList = [];
+
+		if('starter' in menu){
+			menuList.push(model.getSelectedDish('starter'));
+		}
+		if('main' in menu){
+			menuList.push(model.getSelectedDish('main'));
+		}
+		if('dessert' in menu){
+			menuList.push(model.getSelectedDish('dessert'));
+		}
+
+		stuff += "<div class='col-md-3'></div>"
+
+		for (l=0;l<menuList.length;l++){
+				var dish = menuList[l];
+				var price = model.getPrice(dish.id);
+				stuff += "<div class='col-md-12'><div class='row' style='margin-bottom:5%; margin-left:2%;'><div class='col-md-2'>";
+				stuff += "<img src=images/"+dish.image+' width=150px"></div>';
+				stuff += "<div class='col-md-4' style='margin-top:-20px;'><h3>"+dish.name+"</h3>";
+		  		stuff += "<h5>"+model.getAllIngredients(dish.id)+"</h5></div>";
+		  		stuff += "<div class='col-md-4' style='margin-top:-5px;'><h4>Description</h4><h5>"+dish.description+"</h5></div>";
+		  		stuff += '</div></div>';
+	  	}
+
+	  	return stuff;
+	}
+
+	ChosenMenu.html(this.getMenu);
+ 
+}
